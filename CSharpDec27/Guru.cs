a1af782d90d25ac888522a07e031609d3096c28a
ajitdikle <Ajitdikle@1020>
2022-12-28 16:05:23 +0530

    task

--- CSharpDec27/Guru.cs
+++ CSharpDec27/Guru.cs
@@ -19,14 +19,14 @@ namespace SeleniumDec27
                 IWebDriver driver = new ChromeDriver();
                 driver.Url = "https://demo.guru99.com/test/newtours/register.php";
 
-                driver.FindElement(By.Name("firstName")).SendKeys("Povan");
-                driver.FindElement(By.Name("lastName")).SendKeys("coolkarni");
-                driver.FindElement(By.Name("phone")).SendKeys("9964326107");
-                driver.FindElement(By.Id("userName")).SendKeys("Po@gmail.com");
-                driver.FindElement(By.Name("address1")).SendKeys("NavipethPune");
+                driver.FindElement(By.Name("firstName")).SendKeys("rahul");
+                driver.FindElement(By.Name("lastName")).SendKeys("colkarni");
+                driver.FindElement(By.Name("phone")).SendKeys("74833326107");
+                driver.FindElement(By.Id("userName")).SendKeys("rahul@gmail.com");
+                driver.FindElement(By.Name("address1")).SendKeys("Pune");
                 driver.FindElement(By.Name("city")).SendKeys("pune");
                 driver.FindElement(By.Name("state")).SendKeys("Maharatra");
-                driver.FindElement(By.Name("postalCode")).SendKeys("591120");
+                driver.FindElement(By.Name("postalCode")).SendKeys("88920");
 
                 driver.FindElement(By.Name("submit")).Click();
 
